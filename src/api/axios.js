0eb162d70b6661572971f16bd8b57c74e38c932e
Kolliboyina-Asritha <asrithaasritha99@gmail.com>
2026-07-13 22:00:46 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:6006",
+    baseURL: "https://work2-59om.onrender.com",
     headers: {
         "Content-Type": "application/json"
     },
